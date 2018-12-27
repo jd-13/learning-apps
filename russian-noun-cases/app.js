04e20580aefef18a8ef3538a9f1df94b3681268c
jd-13 <jackd8989@gmail.com>
2018-12-27 16:50:58 +0000

    Add multiple choice questions

--- russian-noun-cases/app.js
+++ russian-noun-cases/app.js
@@ -1,74 +1,183 @@
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
 
-/**
- * Contains all the fields that define a generated question.
- */
-class Question {
-    constructor(questionText, answer, feedbackText) {
-        this._questionText = questionText;
-        this._answer = answer;
-        this._feedbackText = feedbackText;
+class BaseQuestion {
+    constructor() {
+        // Do nothing
     }
 
-    get questionText() { return this._questionText; }
-    get answer() { return this._answer; }
-    get feedbackText() { return this._feedbackText; }
+    /**
+     * Inheriting classes must implement this to populate $questionContainer with the HTML elements
+     * required by the question, and implement a call back for one of the elements which will enable
+     * $nextBtn when the user has provided an answer.
+     * 
+     * @param {*} $questionContainer 
+     * @param {*} $nextBtn 
+     * @param {*} $isCorrect 
+     * @param {*} $feedbackLine1 
+     * @param {*} $feedbackLine2 
+     */
+    renderQuestion($questionContainer, $nextBtn, $isCorrect, $feedbackLine1, $feedbackLine2) {
+        throw new BadMethodCallException();
+    }
+}
+
+/**
+ * Generates and renders a question which asks the user to type the correct case.
+ */
+class SimpleQuestion extends BaseQuestion {
+    constructor(dictionary) {
+        super();
+
+        // Choose a noun at random
+        const chosenNoun = dictionary.nouns[Math.floor(Math.random() * dictionary.nouns.length)];
+        
+        // Choose a target case at random, exclude the first case as this will always be nominative
+        const availableCases = Object.keys(chosenNoun).slice(1);
+        const chosenCaseKey = availableCases[Math.floor(Math.random() * availableCases.length)];
+        const chosenCase = chosenNoun[chosenCaseKey];
+
+        // Get the text for the feedback
+        const feedbackLine1 = dictionary.caseRules[chosenCaseKey][chosenCase.caseRule];
+
+        // Check if we need to explain a spelling rule
+        let feedbackLine2 = "";
+        if (chosenCase.hasOwnProperty("spellingRule")) {
+            feedbackLine2 = dictionary.spellingRules[chosenCase.spellingRule];
+        }
+
+        this._questionText = `What is the ${chosenCaseKey} case of ${chosenNoun.nominative.text}?`;
+        this._answer = chosenNoun[chosenCaseKey].text;
+        this._feedbackText = [feedbackLine1, feedbackLine2]
+    }
+
+    renderQuestion($questionContainer, $nextBtn, $isCorrect, $feedbackLine1, $feedbackLine2) {
+        // Add the question's elements to the document
+        let questionHTML = `<div>${this._questionText}</div>`;
+        questionHTML = questionHTML.concat("<div><br>");
+        questionHTML = questionHTML.concat("<input type=\"text\" name=\"answer\" id=\"answerInput\"/></div>")
+        questionHTML = questionHTML.concat("<div><br>");
+        questionHTML = questionHTML.concat("<button type=\"button\" class=\"button\" id=\"submitBtn\">Submit</button></div>")
+
+        $questionContainer.append(questionHTML);
+
+        // Set the submit button handler
+        let that = this;
+        $("#submitBtn").click(function() {
+
+            // Enable the next button
+            $nextBtn.prop("disabled", false);
+
+            if ($("#answerInput").val() === that._answer) {
+                $isCorrect.text("Correct!");
+            } else {
+                $isCorrect.text("Oops!");
+                $feedbackLine1.text(that._feedbackText[0]);
+                $feedbackLine2.text(that._feedbackText[1]);
+            }
+        });
+    }
+}
+
+class CaseChoiceQuestion extends BaseQuestion {
+    constructor(dictionary) {
+        super();
+
+        // Choose a phrase from the dictionary
+        const phrases = dictionary.caseChoicePhrases;
+        const chosenPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+
+        // Choose a noun to substitute into the phrase
+        const nouns = dictionary.nouns;
+        const chosenNoun = nouns[Math.floor(Math.random() * nouns.length)];
+
+        // Lookup the correct case of the noun for this phrase
+        const correctNounCase = chosenNoun[chosenPhrase.targetCase];
+
+        // Pick two other cases at random, exclude the correct case
+        let availableCases = Object.keys(chosenNoun).filter(word => word !== chosenPhrase.targetCase);
+        this._incorrectChoices = [];
+        for (let idx = 0; idx < 2; idx++) {
+            const caseIdx = Math.floor(Math.random() * availableCases.length);
+            this._incorrectChoices.push(chosenNoun[availableCases[caseIdx]].text);
+            availableCases = availableCases.slice(caseIdx);
+        }
+
+        this._questionPhrase = chosenPhrase.text;
+        this._answer = correctNounCase.text;
+    }
+
+    renderQuestion($questionContainer, $nextBtn, $isCorrect, $feedbackLine1, $feedbackLine2) {
+        // Prepare the data
+        const splitPhrase = this._questionPhrase.split("||");
+
+        let shuffledAnswers = [...this._incorrectChoices];
+        shuffledAnswers.push(this._answer);
+        shuffleArray(shuffledAnswers);
+
+        // Add the questions elements to the document
+        let questionHTML = "<div>Choose the correct case for the noun in the below phrase:</div><br>";
+        questionHTML = questionHTML.concat("<div class=\"columns is-vcentered is-centered\">");
+        questionHTML = questionHTML.concat(`<div class=\"column is-narrow\">${splitPhrase[0]}</div>`);
+        questionHTML = questionHTML.concat("<div class=\"column is-narrow\">");
+        shuffledAnswers.forEach(entry => {
+            questionHTML = questionHTML.concat(`<button type=\"button\" class=\"button multiAnswer\">${entry}</button><br>`);
+        });
+        questionHTML = questionHTML.concat("</div>")
+        questionHTML = questionHTML.concat(`<div class=\"column is-narrow\">${splitPhrase[1]}</div>`);
+        questionHTML = questionHTML.concat("</div>");
+
+        $questionContainer.append(questionHTML);
+
+        // Set the button handlers
+        let that = this;
+        $(".multiAnswer").click(function() {
+
+            // Enable the next button
+            $nextBtn.prop("disabled", false);
+
+            if ($(this).text() === that._answer) {
+                $isCorrect.text("Correct!");
+            } else {
+                $isCorrect.text("Oops!");
+                // $feedbackLine1.text(that._feedbackText[0]);
+                // $feedbackLine2.text(that._feedbackText[1]);
+            }
+        });
+    }
 }
 
 /**
  * Randomly generates a new question from the provided dictionary.
  */
 function newQuestion(dictionary) {
+    const questionTypes = [SimpleQuestion, CaseChoiceQuestion];
 
-    // Choose a noun at random
-    const chosenNoun = dictionary.nouns[Math.floor(Math.random() * dictionary.nouns.length)];
-    
-    // Choose a target case at random, exclude the first case as this will always be nominative
-    const availableCases = Object.keys(chosenNoun).slice(1);
-    const chosenCaseKey = availableCases[Math.floor(Math.random() * availableCases.length)];
-    const chosenCase = chosenNoun[chosenCaseKey];
-
-    // Get the text for the feedback
-    const feedbackLine1 = dictionary.caseRules[chosenCaseKey][chosenCase.caseRule];
-
-    // Check if we need to explain a spelling rule
-    let feedbackLine2 = "";
-    if (chosenCase.hasOwnProperty("spellingRule")) {
-        feedbackLine2 = dictionary.spellingRules[chosenCase.spellingRule];
-    }
-
-    return new Question(`What is the ${chosenCaseKey} case of ${chosenNoun.nominative}?`,
-                        chosenNoun[chosenCaseKey].text,
-                        [feedbackLine1, feedbackLine2]);
+    return new questionTypes[Math.floor(Math.random() * questionTypes.length)](dictionary);
 }
 
 const main = function() {
 
     // Collect the necessary elements
-    let $questionText = $("#question");
+    let $questionContainer = $("#questionContainer");
     let $nextBtn = $("#nextBtn");
     let $isCorrect = $("#isCorrect");
     let $feedbackLine1 = $("#feedbackLine1");
     let $feedbackLine2 = $("#feedbackLine2");
-    let $answerInput = $("#answerInput");
 
     // Load the first question
     let question = newQuestion(DICTIONARY);
-    $questionText.text(question.questionText);
-
-    // Set the submit button handler
-    $("#submitBtn").click(function() {
-
-        // Enable the next button
-        $nextBtn.prop("disabled", false);
-
-        if ($answerInput.val() === question.answer) {
-            $isCorrect.text("Correct!");
-        } else {
-            $isCorrect.text("Oops!");
-            $feedbackLine1.text(question.feedbackText[0]);
-            $feedbackLine2.text(question.feedbackText[1]);
-        }
-    });
+    question.renderQuestion($questionContainer,
+                            $nextBtn,
+                            $isCorrect,
+                            $feedbackLine1,
+                            $feedbackLine2);
 
     // Set the next button handler
     $nextBtn.click(function() {
@@ -76,14 +185,18 @@ const main = function() {
         $(this).prop("disabled", true);
 
         // Clear the feedback and text fields
+        $questionContainer.empty();
         $isCorrect.text("");
         $feedbackLine1.text("");
         $feedbackLine2.text("");
-        $answerInput.val("");
 
         // Load the next question
         question = newQuestion(DICTIONARY);
-        $questionText.text(question.questionText);
+        question.renderQuestion($questionContainer,
+                                $nextBtn,
+                                $isCorrect,
+                                $feedbackLine1,
+                                $feedbackLine2);
     });
 }
 
--- russian-noun-cases/libDictionary.js
+++ russian-noun-cases/libDictionary.js
@@ -1,26 +1,29 @@
 DICTIONARY = {
     "nouns": [
         {
-            "nominative": "собака",
+            "nominative": {"text": "собака"},
             "genitive": {"text": "собаки", "caseRule": 2, "spellingRule": 0},
             "accusative": {"text": "собаку", "caseRule": 4},
             "dative": {"text": "собаке", "caseRule": 2},
-            "gender": "f"
         },
         {
-            "nominative": "кошка",
+            "nominative": {"text": "кошка"},
             "genitive": {"text": "кошки", "caseRule": 2, "spellingRule": 0},
             "accusative": {"text": "кошку", "caseRule": 4},
             "dative": {"text": "кошке", "caseRule": 2},
-            "gender": "f"
         },
         {
-            "nominative": "человек",
+            "nominative": {"text": "человек"},
             "genitive": {"text": "человека", "caseRule": 0},
             "accusative": {"text": "человека", "caseRule": 1},
             "dative": {"text": "человеку", "caseRule": 0},
-            "gender": "m"
         },
+        {
+            "nominative": {"text": "женщина"},
+            "genitive": {"text": "женщины", "caseRule": 2},
+            "accusative": {"text": "женщину", "caseRule": 4},
+            "dative": {"text": "женщине", "caseRule": 2}
+        }
     ],
 
     "caseRules": {
@@ -56,5 +59,24 @@ DICTIONARY = {
         "Except here this would put an unstressed о after one of 'ж, ч, ш, щ, ц', so instead use е",
         "Except here this would put я after one of 'г, к, х, ж, ч, ш, щ, ц', so instead use а",
         "Except here this would put ю after one of 'г, к, х, ж, ч, ш, щ, ц', so instead use у"
+    ],
+
+    "caseChoicePhrases": [
+        {
+            "text": "мячь ||",
+            "targetCase": "genitive"
+        },
+        {
+            "text": "дом ||",
+            "targetCase": "genitive"
+        },
+        {
+            "text": "у || есть яблоки",
+            "targetCase": "genitive"
+        },
+        {
+            "text": "у || есть мячь",
+            "targetCase": "genitive"
+        }
     ]
 };
