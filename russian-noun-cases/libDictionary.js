8e2af6c254b9b2cf2e720a4cd873a3d555bd2598
jd-13 <jackd8989@gmail.com>
2019-02-02 10:47:54 +0000

    Added inanimate nouns and phrases

--- russian-noun-cases/libDictionary.js
+++ russian-noun-cases/libDictionary.js
@@ -32,61 +32,83 @@ DICTIONARY = {
             }
         ],
 
-        "inanimate": []
+        "inanimate": [
+            {
+                "nominative": {"text": "чай"},
+                "genitive": {"text": "чая", "caseRule": 1},
+                "accusative": {"text": "чая", "caseRule": 2},
+                "dative": {"text": "чаю", "caseRule": 1},
+                "instrumental": {"text": "чаем", "caseRule": 1}
+            },
+            {
+                "nominative": {"text": "масло"},
+                "genitive": {"text": "масла", "caseRule": 4},
+                "accusative": {"text": "масло", "caseRule": 6},
+                "dative": {"text": "маслу", "caseRule": 4},
+                "instrumental": {"text": "маслом", "caseRule": 6}
+            },
+            {
+                "nominative": {"text": "мяч"},
+                "genitive": {"text": "мяча", "caseRule": 0},
+                "accusative": {"text": "мяч", "caseRule": 0},
+                "dative": {"text": "мячу", "caseRule": 0},
+                "instrumental": {"text": "мячом", "caseRule": 0}
+            }
+        ]
     },
 
     "caseRules": {
         "genitive": [
-            "For masculine nouns ending in a consonant, add а",
-            "For masculine nouns ending in й or ь, replace with я",
-            "For feminine nouns ending in а, replace with ы",
-            "For feminine nouns ending in я or ь, replace with и",
-            "For neuter nouns ending in о, replace with а",
-            "For neuter nouns ending in е, replace with я"
+            /*0*/"For masculine nouns ending in a consonant, add а",
+            /*1*/"For masculine nouns ending in й or ь, replace with я",
+            /*2*/"For feminine nouns ending in а, replace with ы",
+            /*3*/"For feminine nouns ending in я or ь, replace with и",
+            /*4*/"For neuter nouns ending in о, replace with а",
+            /*5*/"For neuter nouns ending in е, replace with я"
         ],
         "accusative": [
-            "For inanimate masculine nouns there is no change",
-            "For animate masculine nouns ending in a consonant, add а",
-            "For animate masculine nouns ending in й, replace with я",
-            "For animate masculine nouns ending in ь, replace with я",
-            "For feminine nouns ending in а, replace with у",
-            "For feminine nouns ending in я, replace with ю",
-            "For inanimate neuter nouns there is no change"
+            /*0*/"For inanimate masculine nouns there is no change",
+            /*1*/"For animate masculine nouns ending in a consonant, add а",
+            /*2*/"For animate masculine nouns ending in й, replace with я",
+            /*3*/"For animate masculine nouns ending in ь, replace with я",
+            /*4*/"For feminine nouns ending in а, replace with у",
+            /*5*/"For feminine nouns ending in я, replace with ю",
+            /*6*/"For inanimate neuter nouns there is no change"
         ],
         "dative": [
-            "For masculine nouns ending in a consonant, add у",
-            "For masculine nouns ending in й or ь, replace with ю",
-            "For feminine nouns ending in а or я, replace with е",
-            "For feminine nouns ending in ь, replace with и",
-            "For neuter nouns ending in о, replace with у",
-            "For neuter nouns ending in е, replace with ю"
+            /*0*/"For masculine nouns ending in a consonant, add у",
+            /*1*/"For masculine nouns ending in й or ь, replace with ю",
+            /*2*/"For feminine nouns ending in а or я, replace with е",
+            /*3*/"For feminine nouns ending in ь, replace with и",
+            /*4*/"For neuter nouns ending in о, replace with у",
+            /*5*/"For neuter nouns ending in е, replace with ю"
         ],
         "instrumental": [
-            "For masculine nouns ending in a consonant (except й), add ом",
-            "For masculine nouns ending in й, replace with eм (ём if stressed)",
-            "For masculing nouns ending in ь, replace with ем (ём if stressed)",
-            "For feminine nouns ending in а, replace with ой (or rarely ою)",
-            "For feminine nouns ending in я, replace with ей (ёй if stressed)",
-            "For feminine nouns ending in ь, add ю",
-            "For neuter nouns add м"
+            /*0*/"For masculine nouns ending in a consonant (except й), add ом",
+            /*1*/"For masculine nouns ending in й, replace with eм (ём if stressed)",
+            /*2*/"For masculing nouns ending in ь, replace with ем (ём if stressed)",
+            /*3*/"For feminine nouns ending in а, replace with ой (or rarely ою)",
+            /*4*/"For feminine nouns ending in я, replace with ей (ёй if stressed)",
+            /*5*/"For feminine nouns ending in ь, add ю",
+            /*6*/"For neuter nouns add м"
         ]
     },
 
     "spellingRules": [
         // Spelling rules for most cases
-        "Except here this would put ы after one of 'г, к, х, ж, ч, ш, щ', so instead use и",
-        "Except here this would put an unstressed о after one of 'ж, ч, ш, щ, ц', so instead use е",
-        "Except here this would put я after one of 'г, к, х, ж, ч, ш, щ, ц', so instead use а",
-        "Except here this would put ю after one of 'г, к, х, ж, ч, ш, щ, ц', so instead use у",
+        /*0*/"Except here this would put ы after one of 'г, к, х, ж, ч, ш, щ', so instead use и",
+        /*1*/"Except here this would put an unstressed о after one of 'ж, ч, ш, щ, ц', so instead use е",
+        /*2*/"Except here this would put я after one of 'г, к, х, ж, ч, ш, щ, ц', so instead use а",
+        /*3*/"Except here this would put ю after one of 'г, к, х, ж, ч, ш, щ, ц', so instead use у",
 
         // Spelling rules for instrumental case
-        "Except here this would put ом after one of 'ж, ч, ш, щ, ц', so instead use ем",
-        "Except here this would put ой after one of 'ж, ч, ш, щ, ц', so instead use ей"
+        /*0*/"Except here this would put ом after one of 'ж, ч, ш, щ, ц', so instead use ем",
+        /*1*/"Except here this would put ой after one of 'ж, ч, ш, щ, ц', so instead use ей"
     ],
 
     "nounChoicePhrases": [
         {
-            "text": "мячь ||",
+            "text": "мяч ||",
             "targetCase": "genitive",
             "nounType": "animate"
         },
@@ -101,9 +123,24 @@ DICTIONARY = {
             "nounType": "animate"
         },
         {
-            "text": "у || есть мячь",
+            "text": "у || есть мяч",
             "targetCase": "genitive",
             "nounType": "animate"
+        },
+        {
+            "text": "|| пожалуйста",
+            "targetCase": "nominative",
+            "nounType": "inanimate"
+        },
+        {
+            "text": "у неё нет ||",
+            "targetCase": "genitive",
+            "nounType": "inanimate"
+        },
+        {
+            "text": "у ребёнка нет ||",
+            "targetCase": "genitive",
+            "nounType": "inanimate"
         }
     ],
 
