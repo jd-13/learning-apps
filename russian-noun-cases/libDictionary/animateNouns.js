5fed69fdff443fd718e768d960fbf2513133bb4d
jd-13 <jackd8989@gmail.com>
2020-01-23 20:54:55 +0000

    Split nouns into separate files

---
+++ russian-noun-cases/libDictionary/animateNouns.js
@@ -0,0 +1,51 @@
+ANIMATE_NOUNS = [
+    // We're not including gender exceptions such as папа for now
+    {
+        "nominative": {"text": "собака"},
+        "genitive": {"text": "собаки", "caseRule": 2, "spellingRule": 0},
+        "accusative": {"text": "собаку", "caseRule": 4},
+        "dative": {"text": "собаке", "caseRule": 2},
+        "instrumental": {"text": "собакой", "caseRule": 3},
+        "prepositional": {"text": "собаке", "caseRule": 2}
+    },
+    {
+        "nominative": {"text": "кошка"},
+        "genitive": {"text": "кошки", "caseRule": 2, "spellingRule": 0},
+        "accusative": {"text": "кошку", "caseRule": 4},
+        "dative": {"text": "кошке", "caseRule": 2},
+        "instrumental": {"text": "кошкой", "caseRule": 3},
+        "prepositional": {"text": "кошке", "caseRule": 2}
+    },
+    {
+        "nominative": {"text": "человек"},
+        "genitive": {"text": "человека", "caseRule": 0},
+        "accusative": {"text": "человека", "caseRule": 1},
+        "dative": {"text": "человеку", "caseRule": 0},
+        "instrumental": {"text": "человеком", "caseRule": 0},
+        "prepositional": {"text": "человеке", "caseRule": 0}
+    },
+    {
+        "nominative": {"text": "женщина"},
+        "genitive": {"text": "женщины", "caseRule": 2},
+        "accusative": {"text": "женщину", "caseRule": 4},
+        "dative": {"text": "женщине", "caseRule": 2},
+        "instrumental": {"text": "женщиной", "caseRule": 3},
+        "prepositional": {"text": "женщине", "caseRule": 2}
+    },
+    {
+        "nominative": {"text": "медведь"},
+        "genitive": {"text": "медведя", "caseRule": 1},
+        "accusative": {"text": "медведя", "caseRule": 3},
+        "dative": {"text": "медведю", "caseRule": 1},
+        "instrumental": {"text": "медведем", "caseRule": 2},
+        "prepositional": {"text": "медведе", "caseRule": 1}
+    },
+    {
+        "nominative": {"text": "змея"},
+        "genitive": {"text": "змеи", "caseRule": 3},
+        "accusative": {"text": "змею", "caseRule": 5},
+        "dative": {"text": "змее", "caseRule": 2},
+        "instrumental": {"text": "змеёй", "caseRule": 4},
+        "prepositional": {"text": "змее", "caseRule": 2}
+    }
+]
---
+++ russian-noun-cases/libDictionary/inanimateNouns.js
@@ -0,0 +1,82 @@
+INANIMATE_NOUNS = [
+    {
+        "nominative": {"text": "чай"},
+        "genitive": {"text": "чая", "caseRule": 1},
+        "accusative": {"text": "чая", "caseRule": 2},
+        "dative": {"text": "чаю", "caseRule": 1},
+        "instrumental": {"text": "чаем", "caseRule": 1},
+        "prepositional": {"text": "чае", "caseRule": 2}
+    },
+    {
+        "nominative": {"text": "масло"},
+        "genitive": {"text": "масла", "caseRule": 4},
+        "accusative": {"text": "масло", "caseRule": 6},
+        "dative": {"text": "маслу", "caseRule": 4},
+        "instrumental": {"text": "маслом", "caseRule": 6},
+        "prepositional": {"text": "масле", "caseRule": 4}
+    },
+    {
+        "nominative": {"text": "мяч"},
+        "genitive": {"text": "мяча", "caseRule": 0},
+        "accusative": {"text": "мяч", "caseRule": 0},
+        "dative": {"text": "мячу", "caseRule": 0},
+        "instrumental": {"text": "мячом", "caseRule": 0},
+        "prepositional": {"text": "мяче", "caseRule": 0}
+    },
+    {
+        "nominative": {"text": "дом"},
+        "genitive": {"text": "дома", "caseRule": 0},
+        "accusative": {"text": "дом", "caseRule": 0},
+        "dative": {"text": "дому", "caseRule": 0},
+        "instrumental": {"text": "домом", "caseRule": 0},
+        "prepositional": {"text": "доме", "caseRule": 0}
+    },
+    {
+        "nominative": {"text": "коробка"},
+        "genitive": {"text": "коробки", "caseRule": 2, "spellingRule": 0},
+        "accusative": {"text": "коробку", "caseRule": 4},
+        "dative": {"text": "коробке", "caseRule": 2},
+        "instrumental": {"text": "коробкой", "caseRule": 3},
+        "prepositional": {"text": "коробке", "caseRule": 2}
+    },
+    {
+        "nominative": {"text": "вода"},
+        "genitive": {"text": "воды", "caseRule": 2},
+        "accusative": {"text": "воду", "caseRule": 4},
+        "dative": {"text": "воде", "caseRule": 2},
+        "instrumental": {"text": "водой", "caseRule": 1},
+        "prepositional": {"text": "воде", "caseRule": 2}
+    },
+    {
+        "nominative": {"text": "парк"},
+        "genitive": {"text": "парка", "caseRule": 0},
+        "accusative": {"text": "парк", "caseRule": 0},
+        "dative": {"text": "парку", "caseRule": 0},
+        "instrumental": {"text": "парком", "caseRule": 0},
+        "prepositional": {"text": "парке", "caseRule": 0}
+    },
+    {
+        "nominative": {"text": "тарелка"},
+        "genitive": {"text": "тарелки", "caseRule": 2, "spellingRule": 0},
+        "accusative": {"text": "тарелку", "caseRule": 4},
+        "dative": {"text": "тарелке", "caseRule": 2},
+        "instrumental": {"text": "тарелкой", "caseRule": 3},
+        "prepositional": {"text": "тарелке", "caseRule": 2}
+    },
+    {
+        "nominative": {"text": "город"},
+        "genitive": {"text": "города", "caseRule": 0},
+        "accusative": {"text": "город", "caseRule": 0},
+        "dative": {"text": "городу", "caseRule": 0},
+        "instrumental": {"text": "городом", "caseRule": 0},
+        "prepositional": {"text": "городе", "caseRule": 0}
+    },
+    {
+        "nominative": {"text": "дерево"},
+        "genitive": {"text": "дерева", "caseRule": 4},
+        "accusative": {"text": "дерево", "caseRule": 6},
+        "dative": {"text": "дереву", "caseRule": 4},
+        "instrumental": {"text": "деревом", "caseRule": 6},
+        "prepositional": {"text": "дереве", "caseRule": 4}
+    }
+]
