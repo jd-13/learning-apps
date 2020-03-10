e687ddf7d5ca0eb34851a11312fc6b06fea3e5a7
jd-13 <jackd8989@gmail.com>
2020-03-10 08:34:29 +0000

    Add first few adjectives

---
+++ russian-noun-cases/libDictionary/adjectives.js
@@ -0,0 +1,44 @@
+ADJECTIVES = [
+    {
+        "masculine": {
+            "nominative": "красивый"
+        },
+        "feminine": {
+            "nominative": "красивая"
+        },
+        "neuter": {
+            "nominative": "красивое"
+        },
+        "plural": {
+            "nominative": "красивые"
+        }
+    },
+    {
+        "masculine": {
+            "nominative": "большой"
+        },
+        "feminine": {
+            "nominative": "большая"
+        },
+        "neuter": {
+            "nominative": "большое"
+        },
+        "plural": {
+            "nominative": "большие"
+        }
+    },
+    {
+        "masculine": {
+            "nominative": "хороший"
+        },
+        "feminine": {
+            "nominative": "хорошая"
+        },
+        "neuter": {
+            "nominative": "хорошее"
+        },
+        "plural": {
+            "nominative": "хорошие"
+        }
+    }
+]
