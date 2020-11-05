6ba78d0400a1d48945cab311d5dfebe4b97d30d6
jd-13 <jackd8989@gmail.com>
2020-11-05 21:43:32 +0000

    Alphabetise countries

--- russian/countries/libCountries.js
+++ russian/countries/libCountries.js
@@ -3,54 +3,14 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 COUNTRIES = [{
-    "flag": "https://upload.wikimedia.org/wikipedia/en/f/f3/Flag_of_Russia.svg",
-    "country": "Россия",
-    "genitive": "России",
-    "languages": ["русски"],
+    "flag": "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg",
+    "country": "Америка",
+    "genitive": "Америки",
+    "languages": ["английски"],
     "nationality": {
-        "masculine": "русскии",
-        "feminine": "русская",
-        "plural": "русские"
-    }
-}, {
-    "flag": "https://upload.wikimedia.org/wikipedia/commons/0/08/Flag_of_Switzerland_%28Pantone%29.svg",
-    "country": "Швейцария",
-    "genitive": "Швейцарии",
-    "languages": ["немецки"],
-    "nationality": {
-        "masculine": "швейцарец",
-        "feminine": "швейцарка",
-        "plural": "швейцарцы"
-    }
-}, {
-    "flag": "https://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg",
-    "country": "Италия",
-    "genitive": "Италии",
-    "languages": ["итальянски"],
-    "nationality": {
-        "masculine": "итальянец",
-        "feminine": "итальянка",
-        "plural": "итальянцы"
-    }
-}, {
-    "flag": "https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg",
-    "country": "Франция",
-    "genitive": "Франции",
-    "languages": ["французски"],
-    "nationality": {
-        "masculine": "француз",
-        "feminine": "француженка",
-        "plural": "французы"
-    }
-}, {
-    "flag": "https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg",
-    "country": "Германия",
-    "genitive": "Германии",
-    "languages": ["немецки"],
-    "nationality": {
-        "masculine": "немец",
-        "feminine": "немка",
-        "plural": "немцы"
+        "masculine": "американец",
+        "feminine": "американка",
+        "plural": "американцы"
     }
 }, {
     "flag": "https://upload.wikimedia.org/wikipedia/en/b/be/Flag_of_England.svg",
@@ -63,6 +23,26 @@ COUNTRIES = [{
         "plural": "англичане"
     }
 }, {
+    "flag": "https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg",
+    "country": "Аргентина",
+    "genitive": "Аргентины",
+    "languages": ["испански"],
+    "nationality": {
+        "masculine": "аргентинец",
+        "feminine": "аргентинка",
+        "plural": "аргентинцы"
+    }
+}, {
+    "flag": "https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg",
+    "country": "Германия",
+    "genitive": "Германии",
+    "languages": ["немецки"],
+    "nationality": {
+        "masculine": "немец",
+        "feminine": "немка",
+        "plural": "немцы"
+    }
+}, {
     "flag": "https://upload.wikimedia.org/wikipedia/en/9/9a/Flag_of_Spain.svg",
     "country": "Испания",
     "genitive": "Испании",
@@ -73,34 +53,14 @@ COUNTRIES = [{
         "plural": "испанцы"
     }
 }, {
-    "flag": "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg",
-    "country": "Америка",
-    "genitive": "Америки",
-    "languages": ["английски"],
+    "flag": "https://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg",
+    "country": "Италия",
+    "genitive": "Италии",
+    "languages": ["итальянски"],
     "nationality": {
-        "masculine": "американец",
-        "feminine": "американка",
-        "plural": "американцы"
-    }
-}, {
-    "flag": "https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg",
-    "country": "Мексика",
-    "genitive": "Мексики",
-    "languages": ["испански"],
-    "nationality": {
-        "masculine": "мексиканец",
-        "feminine": "мексиканка",
-        "plural": "мексиканцы"
-    }
-}, {
-    "flag": "https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg",
-    "country": "Аргентина",
-    "genitive": "Аргентины",
-    "languages": ["испански"],
-    "nationality": {
-        "masculine": "аргентинец",
-        "feminine": "аргентинка",
-        "plural": "аргентинцы"
+        "masculine": "итальянец",
+        "feminine": "итальянка",
+        "plural": "итальянцы"
     }
 }, {
     "flag": "https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg",
@@ -121,6 +81,46 @@ COUNTRIES = [{
         "masculine": "китаец",
         "feminine": "китаянка",
         "plural": "китайцы"
+    }
+}, {
+    "flag": "https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg",
+    "country": "Мексика",
+    "genitive": "Мексики",
+    "languages": ["испански"],
+    "nationality": {
+        "masculine": "мексиканец",
+        "feminine": "мексиканка",
+        "plural": "мексиканцы"
+    }
+}, {
+    "flag": "https://upload.wikimedia.org/wikipedia/en/f/f3/Flag_of_Russia.svg",
+    "country": "Россия",
+    "genitive": "России",
+    "languages": ["русски"],
+    "nationality": {
+        "masculine": "русскии",
+        "feminine": "русская",
+        "plural": "русские"
+    }
+}, {
+    "flag": "https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg",
+    "country": "Франция",
+    "genitive": "Франции",
+    "languages": ["французски"],
+    "nationality": {
+        "masculine": "француз",
+        "feminine": "француженка",
+        "plural": "французы"
+    }
+}, {
+    "flag": "https://upload.wikimedia.org/wikipedia/commons/0/08/Flag_of_Switzerland_%28Pantone%29.svg",
+    "country": "Швейцария",
+    "genitive": "Швейцарии",
+    "languages": ["немецки"],
+    "nationality": {
+        "masculine": "швейцарец",
+        "feminine": "швейцарка",
+        "plural": "швейцарцы"
     }
 }];
 
--- russian/countries/src/libCountries.js
+++ russian/countries/src/libCountries.js
@@ -1,57 +1,13 @@
 COUNTRIES = [
     {
-        "flag": "https://upload.wikimedia.org/wikipedia/en/f/f3/Flag_of_Russia.svg",
-        "country": "Россия",
-        "genitive": "России",
-        "languages": ["русски"],
+        "flag": "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg",
+        "country": "Америка",
+        "genitive": "Америки",
+        "languages": ["английски"],
         "nationality": {
-            "masculine": "русскии",
-            "feminine": "русская",
-            "plural": "русские"
-        }
-    },
-    {
-        "flag": "https://upload.wikimedia.org/wikipedia/commons/0/08/Flag_of_Switzerland_%28Pantone%29.svg",
-        "country": "Швейцария",
-        "genitive": "Швейцарии",
-        "languages": ["немецки"],
-        "nationality": {
-            "masculine": "швейцарец",
-            "feminine": "швейцарка",
-            "plural": "швейцарцы"
-        }
-    },
-    {
-        "flag": "https://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg",
-        "country": "Италия",
-        "genitive": "Италии",
-        "languages": ["итальянски"],
-        "nationality": {
-            "masculine": "итальянец",
-            "feminine": "итальянка",
-            "plural": "итальянцы"
-        }
-    },
-    {
-        "flag": "https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg",
-        "country": "Франция",
-        "genitive": "Франции",
-        "languages": ["французски"],
-        "nationality": {
-            "masculine": "француз",
-            "feminine": "француженка",
-            "plural": "французы"
-        }
-    },
-    {
-        "flag": "https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg",
-        "country": "Германия",
-        "genitive": "Германии",
-        "languages": ["немецки"],
-        "nationality": {
-            "masculine": "немец",
-            "feminine": "немка",
-            "plural": "немцы"
+            "masculine": "американец",
+            "feminine": "американка",
+            "plural": "американцы"
         }
     },
     {
@@ -66,6 +22,28 @@ COUNTRIES = [
         }
     },
     {
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg",
+        "country": "Аргентина",
+        "genitive": "Аргентины",
+        "languages": ["испански"],
+        "nationality": {
+            "masculine": "аргентинец",
+            "feminine": "аргентинка",
+            "plural": "аргентинцы"
+        }
+    },
+    {
+        "flag": "https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg",
+        "country": "Германия",
+        "genitive": "Германии",
+        "languages": ["немецки"],
+        "nationality": {
+            "masculine": "немец",
+            "feminine": "немка",
+            "plural": "немцы"
+        }
+    },
+    {
         "flag": "https://upload.wikimedia.org/wikipedia/en/9/9a/Flag_of_Spain.svg",
         "country": "Испания",
         "genitive": "Испании",
@@ -77,36 +55,14 @@ COUNTRIES = [
         }
     },
     {
-        "flag": "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg",
-        "country": "Америка",
-        "genitive": "Америки",
-        "languages": ["английски"],
+        "flag": "https://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg",
+        "country": "Италия",
+        "genitive": "Италии",
+        "languages": ["итальянски"],
         "nationality": {
-            "masculine": "американец",
-            "feminine": "американка",
-            "plural": "американцы"
-        }
-    },
-    {
-        "flag": "https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg",
-        "country": "Мексика",
-        "genitive": "Мексики",
-        "languages": ["испански"],
-        "nationality": {
-            "masculine": "мексиканец",
-            "feminine": "мексиканка",
-            "plural": "мексиканцы"
-        }
-    },
-    {
-        "flag": "https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg",
-        "country": "Аргентина",
-        "genitive": "Аргентины",
-        "languages": ["испански"],
-        "nationality": {
-            "masculine": "аргентинец",
-            "feminine": "аргентинка",
-            "plural": "аргентинцы"
+            "masculine": "итальянец",
+            "feminine": "итальянка",
+            "plural": "итальянцы"
         }
     },
     {
@@ -129,6 +85,50 @@ COUNTRIES = [
             "masculine": "китаец",
             "feminine": "китаянка",
             "plural": "китайцы"
+        }
+    },
+    {
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg",
+        "country": "Мексика",
+        "genitive": "Мексики",
+        "languages": ["испански"],
+        "nationality": {
+            "masculine": "мексиканец",
+            "feminine": "мексиканка",
+            "plural": "мексиканцы"
+        }
+    },
+    {
+        "flag": "https://upload.wikimedia.org/wikipedia/en/f/f3/Flag_of_Russia.svg",
+        "country": "Россия",
+        "genitive": "России",
+        "languages": ["русски"],
+        "nationality": {
+            "masculine": "русскии",
+            "feminine": "русская",
+            "plural": "русские"
+        }
+    },
+    {
+        "flag": "https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg",
+        "country": "Франция",
+        "genitive": "Франции",
+        "languages": ["французски"],
+        "nationality": {
+            "masculine": "француз",
+            "feminine": "француженка",
+            "plural": "французы"
+        }
+    },
+    {
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/0/08/Flag_of_Switzerland_%28Pantone%29.svg",
+        "country": "Швейцария",
+        "genitive": "Швейцарии",
+        "languages": ["немецки"],
+        "nationality": {
+            "masculine": "швейцарец",
+            "feminine": "швейцарка",
+            "plural": "швейцарцы"
         }
     }
 ]
